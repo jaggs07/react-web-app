676246855dd9bf7803975a0aff1998296c40a912
jaggs007 <jagaran.shrestha9@gmail.com>
2017-03-15 19:01:05 +0545

    web page layout

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class NavBar extends Component {
+
+	render() {
+
+		return (
+				<div class="well">
+		             <img class="logo" src="http://getrake.io/wp-content/uploads/2016/08/RakeLogoWebsite.png"/>
+ 				</div>
+		);
+	}
+}
+
+export default NavBar;
---
+++ src/components/style.js
@@ -0,0 +1,3 @@
+module.exports ={
+	marginTop : "24px"
+}
